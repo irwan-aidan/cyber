6831242e1720be36341539a51d10e6e9dfb37d68
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-26 03:02:29 +0800

    Update nginx.sh

--- v2ray/nginx.sh
+++ v2ray/nginx.sh
@@ -1,65 +1,256 @@
 domain=$(cat /root/domain)
-apt install -y nginx
-mkdir /root/.acme.sh
-curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
-chmod +x /root/.acme.sh/acme.sh
-~/.acme.sh/acme.sh  --issue  -d $domain  --webroot /usr/share/nginx/html/
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+apt-get update && apt-get -y upgrade
+apt-get -y install nginx socat
+apt-get install netfilter-persistent -y
+apt install curl xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion ntpdate -y
+ntpdate pool.ntp.org
+apt -y install chrony
+timedatectl set-ntp true
+systemctl enable chronyd && systemctl restart chronyd
+systemctl enable chrony && systemctl restart chrony
+timedatectl set-timezone Asia/Kuala_Lumpur
+chronyc sourcestats -v
+chronyc tracking -v
+date
+
+hostnamectl set-hostname $domain
+bash <(curl -L -s https://install.direct/go.sh)
+curl https://get.acme.sh | sh
+systemctl stop nginx
+~/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
-service squid start
+uuid=$(cat /proc/sys/kernel/random/uuid)
 
-cat > /etc/nginx/nginx.conf <<-EOF
-user  root;
-worker_processes  1;
-#error_log  /etc/nginx/error.log warn;
-#pid    /var/run/nginx.pid;
-events {
-    worker_connections  1024;
-}
-http {
-    include       /etc/nginx/mime.types;
-    default_type  application/octet-stream;
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status \$body_bytes_sent "\$http_referer" '
-                      '"\$http_user_agent" "\$http_x_forwarded_for"';
-    #access_log  /etc/nginx/access.log  main;
-    sendfile        on;
-    #tcp_nopush     on;
-    keepalive_timeout  120;
-    client_max_body_size 20m;
-    #gzip  on;
-    include /etc/nginx/conf.d/*.conf;
+cat <<EOF >>/etc/nginx/sites-available/ssl
+server {
+    listen 443 ssl default_server;
+    listen [::]:443 ssl default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    ssl on;
+    ssl_certificate       /etc/v2ray/v2ray.crt;
+    ssl_certificate_key   /etc/v2ray/v2ray.key;
+    ssl_protocols         TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers           HIGH:!aNULL:!MD5;
+    server_name           $domain;
+    location /v2ray/ {
+        proxy_redirect off;
+        proxy_pass http://127.0.0.1:10000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
 }
 EOF
+ln -s /etc/nginx/sites-available/ssl /etc/nginx/sites-enabled/
+rm -f /etc/v2ray/config.json
 
-cat > /etc/nginx/conf.d/default.conf<<-EOF
- server {
-    listen       127.0.0.1:37212;
-    server_name  $domain;
-    root /usr/share/nginx/html;
-    index index.php index.html index.htm;
+cat> /etc/v2ray/config.json << END
+{
+  "log": {
+    "access": "/var/log/v2ray/access.log",
+    "error": "/var/log/v2ray/error.log",
+    "loglevel": "info"
+  },
+  "inbounds": [
+    {
+      "port": 10000,
+      "listen":"127.0.0.1",
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "${uuid}",
+            "alterId": 32
+#tls
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "tlsSettings": {
+          "certificates": [
+            {
+              "certificateFile": "etc/v2ray/v2ray.crt",
+              "keyFile": "/etc/v2ray/v2ray.key"
+            }
+          ]
+        },
+        "wsSettings": {
+          "path": "/v2ray/",
+          "headers": {
+            "Host": ""
+          }
+         },
+        "quicSettings": {},
+        "sockopt": {
+          "mark": 0,
+          "tcpFastOpen": true
+        }
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      },
+      "domain": "$domain"
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "routing": {
+    "rules": [
+      {
+        "type": "field",
+        "ip": [
+          "0.0.0.0/8",
+          "10.0.0.0/8",
+          "100.64.0.0/10",
+          "169.254.0.0/16",
+          "172.16.0.0/12",
+          "192.0.0.0/24",
+          "192.0.2.0/24",
+          "192.168.0.0/16",
+          "198.18.0.0/15",
+          "198.51.100.0/24",
+          "203.0.113.0/24",
+          "::1/128",
+          "fc00::/7",
+          "fe80::/10"
+        ],
+        "outboundTag": "blocked"
+      },
+      {
+        "type": "field",
+        "outboundTag": "blocked",
+        "protocol": [
+          "bittorrent"
+        ]
+      }
+    ]
+  }
 }
- server {
-    listen       127.0.0.1:37213 http2;
-    server_name  $domain;
-    root /usr/share/nginx/html;
-    index index.php index.html index.htm;
+END
+cat> /etc/v2ray/none.json << END
+{
+  "log": {
+    "access": "/var/log/v2ray/access.log",
+    "error": "/var/log/v2ray/error.log",
+    "loglevel": "info"
+  },
+  "inbounds": [
+    {
+      "port": 10000,
+      "listen":"127.0.0.1",
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "${uuid}",
+            "alterId": 32
+#none
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/v2ray/",
+          "headers": {
+            "Host": ""
+          }
+         },
+        "quicSettings": {},
+        "sockopt": {
+          "mark": 0,
+          "tcpFastOpen": true
+        }
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      },
+      "domain": "$domain"
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "routing": {
+    "rules": [
+      {
+        "type": "field",
+        "ip": [
+          "0.0.0.0/8",
+          "10.0.0.0/8",
+          "100.64.0.0/10",
+          "169.254.0.0/16",
+          "172.16.0.0/12",
+          "192.0.0.0/24",
+          "192.0.2.0/24",
+          "192.168.0.0/16",
+          "198.18.0.0/15",
+          "198.51.100.0/24",
+          "203.0.113.0/24",
+          "::1/128",
+          "fc00::/7",
+          "fe80::/10"
+        ],
+        "outboundTag": "blocked"
+      },
+      {
+        "type": "field",
+        "outboundTag": "blocked",
+        "protocol": [
+          "bittorrent"
+        ]
+      }
+    ]
+  }
 }
-    
-server { 
-    listen       0.0.0.0:80;
-    server_name  $domain;
-    root /usr/share/nginx/html/;
-    index index.php index.html;
-    #rewrite ^(.*)$  https://\$host\$1 permanent; 
-}
-EOF
+END
 
-systemctl enable nginx.service
-systemctl restart nginx.service
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+systemctl daemon-reload
+systemctl enable v2ray@none.service
+systemctl start v2ray@none.service
+systemctl restart nginx
+systemctl enable nginx
+systemctl restart v2ray
+systemctl enable v2ray
 
-cd /usr/share/nginx/html/ && rm -f ./*
-wget https://github.com/atrandys/trojan/raw/master/fakesite.zip
-unzip -o fakesite.zip
-rm -f /root/fakesite.zip
-cd
+
