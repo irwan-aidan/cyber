378232ba9a14311bd4adbc3a6965ad19e624f338
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-26 06:18:58 +0800

    Update vrayku.sh

--- v2ray/vrayku.sh
+++ v2ray/vrayku.sh
@@ -59,27 +59,31 @@ systemctl stop nginx
 uuid=$(cat /proc/sys/kernel/random/uuid)
 cat <<EOF >>/etc/nginx/sites-available/ssl
 server {
-    listen 443 ssl default_server;
-    listen [::]:443 ssl default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
+    listen 443 ssl;
     ssl on;
-    ssl_certificate       /etc/v2ray/v2ray.crt;
-    ssl_certificate_key   /etc/v2ray/v2ray.key;
+    ssl_certificate       /usr/local/etc/v2ray/v2ray.crt;
+    ssl_certificate_key   /usr/local/etc/v2ray/v2ray.key;
     ssl_protocols         TLSv1 TLSv1.1 TLSv1.2;
     ssl_ciphers           HIGH:!aNULL:!MD5;
     server_name           $domain;
-    location /ws/ {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:10000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$http_host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    index index.html index.htm;
+    root  /var/www/sCalc;
+    error_page 400 = /400.html;
+    location /ws/ 
+    {
+    proxy_redirect off;
+    proxy_pass http://127.0.0.1:10000;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection "upgrade";
+    proxy_set_header Host \$http_host;
     }
 }
+server {
+    listen 80;
+    server_name $domain;
+    return 301 https://use.shadowsocksr.win\$request_uri;
+    }
 EOF
 ln -s /etc/nginx/sites-available/ssl /etc/nginx/sites-enabled/
 rm -f /etc/v2ray/config.json
