df4e49e86a146fb94e1e2952cf81d768682a0bc4
kor8 <87409181+kor8@users.noreply.github.com>
2021-11-27 08:01:13 +0800

    Update vrayku.sh

--- v2ray/vrayku.sh
+++ v2ray/vrayku.sh
@@ -83,7 +83,7 @@ server {
 EOF
 ln -s /etc/nginx/sites-available/ssl /etc/nginx/sites-enabled/
 rm -f /etc/v2ray/config.json
-cat <<END >>/etc/v2ray/config.json
+cat <<EOF >>/etc/v2ray/config.json
 {
   "log": {
     "access": "/var/log/v2ray/access.log",
@@ -99,79 +99,29 @@ cat <<END >>/etc/v2ray/config.json
         "clients": [
           {
             "id": "b831381d-6324-4d53-ad4f-8cda48b30811",
-            "alterId": 32
-#tls
+            "alterId": 64,
+            "security": "auto",
+            "level": 0
+#tls            
           }
         ]
       },
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/ws/",
-          "headers": {
-            "Host": ""
-          }
-         },
-        "quicSettings": {},
-        "sockopt": {
-          "mark": 0,
-          "tcpFastOpen": true
+          "path": "/ws/"
         }
-      },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": [
-          "http",
-          "tls"
-        ]
-      },
-      "domain": "dani.ovh.xvolt.tech"
+      }
     }
   ],
   "outbounds": [
     {
       "protocol": "freedom",
       "settings": {}
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
     }
-  ],
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "blocked"
-      },
-      {
-        "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
-      }
-    ]
-  }
+  ]
 }
-END
+EOF
 iptables -A INPUT -p tcp  --match multiport --dports 443,80 -j ACCEPT
 iptables -A INPUT -p udp  --match multiport --dports 443,80 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
