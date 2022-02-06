c28ac57f97baeb37df3ac20c29a8c10e2855fee2
kor8 <87409181+kor8@users.noreply.github.com>
2022-02-07 01:08:33 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,15 @@
+##!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "         Premium Auto Script By PAKYAVPN         " | lolcat
+echo -e "-----------------------------------------------" | lolcat
+echo -e ""
+figlet PakyaVPN | lolcat
+figlet AutoScript | lolcat
+echo "         	        UNLIMITED SEGALANYA!              " | lolcat
+echo ""
+echo "         ......................................     " | lolcat
+echo "                      Build in 2022                 " | lolcat
+echo "                  TELEGRAM : @anakjati567               " | lolcat
+echo -e "=================================================" | lolcat
