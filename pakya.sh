f10b6355b9b3fb02b73257b044f961cb7213967a
kor8 <87409181+kor8@users.noreply.github.com>
2022-02-07 02:38:31 +0800

    Update pakya.sh

--- pakya.sh
+++ pakya.sh
@@ -1,15 +1,13 @@
-##!/bin/bash
+#!/bin/bash
 
 clear
 echo -e "=================================================" | lolcat
-echo -e "         Premium Auto Script By PAKYAVPN         " | lolcat
-echo -e "-----------------------------------------------" | lolcat
-echo -e ""
-figlet "PakyaVPN" | lolcat
-figlet "AutoScript" | lolcat
-echo -e "         	        UNLIMITED SEGALANYA!              " | lolcat
-echo -e ""
-echo -e "         ......................................     " | lolcat
-echo -e "                      Build in 2022                 " | lolcat
-echo -e "                  TELEGRAM : @anakjati567               " | lolcat
+echo -e "#           AutoScript VPS              #"
+echo -e "#-----------------------------------------------#"
+echo -e "#             OPERATING SYSTEM                  #"
+echo -e "             -------------------                 "
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "#-----------------------------------------------#"
 echo -e "=================================================" | lolcat
